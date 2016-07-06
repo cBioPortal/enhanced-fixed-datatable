e006cd3a4aad389031ea9b4f2f394072d32a32bf
Hongxin Zhang <jackson.zhang.828@gmail.com>
2016-07-06 18:20:44 -0400

    Hide Select button after making selection

--- app/scripts/js/EnhancedFixedDatatable.js
+++ app/scripts/js/EnhancedFixedDatatable.js
@@ -1472,7 +1472,7 @@ var EnhancedFixedDataTableSpecial = (function() {
           ), 
           React.createElement("div", {className: "EFDT-finish-selection-button"}, 
             
-              (['mutatedGene', 'cna'].indexOf(this.props.tableType) !== -1 && this.state.selectedRows.length > 0) ?
+              (['mutatedGene', 'cna'].indexOf(this.props.tableType) !== -1 && this.state.selectedRows.length > 0 && this.state.confirmedRows.length !== this.state.selectedRows.length ) ?
                 React.createElement("button", {className: "btn btn-default btn-xs", onClick: this.state.selectButtonClickCallback}, "Select") : ''
             
           )
