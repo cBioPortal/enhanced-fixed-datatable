eef183c20caba7e5b3f789514752b295a66a4fbc
Hongxin Zhang <hongxin@cbio.mskcc.org>
2018-03-30 10:37:06 -0400

    Update js file

--- app/scripts/js/EnhancedFixedDatatable.js
+++ app/scripts/js/EnhancedFixedDatatable.js
@@ -111,6 +111,7 @@ window.EnhancedFixedDataTableSpecial = (function() {
         var _content = this.props.content();
 
         // The current not official limitation is 1,000,000
+        // https://github.com/zeroclipboard/zeroclipboard/issues/529
         if (!_.isString(_content) || _content.length > 1000000) {
           _show = false;
         }
